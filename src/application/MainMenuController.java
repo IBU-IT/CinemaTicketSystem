15387f6493c33243521815f991e2c002207faf6c
ecogo2 <emircogo@hotmail.com>
2017-01-07 22:24:14 +0100

    progress, woo!

--- src/application/MainMenuController.java
+++ src/application/MainMenuController.java
@@ -1,22 +1,31 @@
 package application;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class MainMenuController implements Initializable {
 
-	@FXML private Button addMovie = new Button();
+	@FXML private Button updateDatabase = new Button();
 	@FXML private Button reserveSeat = new Button();
 	@FXML private TextField textField = new TextField();
 	
-	public void onAddMovieClick(ActionEvent event){
-		
+	Parent root;
+	Scene sc;
+	
+	public void onUpdateClick(ActionEvent event) throws IOException{
+		sc=(Scene) updateDatabase.getScene();
+		root=FXMLLoader.load(getClass().getResource("Update.fxml"));
+		sc.setRoot(root);
 	}
 	
 	public void onReserveSeatClick(ActionEvent event){
---
+++ src/application/UpdateController.java
@@ -0,0 +1,63 @@
+package application;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+
+public class UpdateController implements Initializable{
+
+	@FXML private Button updateMovies = new Button();
+	@FXML private Button updateVenues = new Button();
+	@FXML private Button updateUsers = new Button();
+	@FXML private Button back = new Button();
+	
+	Parent root;
+	Scene sc;
+	
+	
+	public void onUpdateMoviesClick(ActionEvent event) throws IOException{
+		
+		sc=(Scene) updateMovies.getScene();
+		root=FXMLLoader.load(getClass().getResource("UpdateMovies.fxml"));
+		sc.setRoot(root);
+		
+	}
+	
+	public void onUpdateVenuesClick(ActionEvent event) throws IOException{
+			
+			sc=(Scene) updateMovies.getScene();
+			root=FXMLLoader.load(getClass().getResource("UpdateVenues.fxml"));
+			sc.setRoot(root);
+			
+		}
+	
+	public void onUpdateUsersClick(ActionEvent event) throws IOException{
+		
+		sc=(Scene) updateMovies.getScene();
+		root=FXMLLoader.load(getClass().getResource("UpdateUsers.fxml"));
+		sc.setRoot(root);
+	
+	}
+	
+	public void onBackClick(ActionEvent event) throws IOException{
+			
+			sc=(Scene) updateMovies.getScene();
+			root=FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
+			sc.setRoot(root);
+			
+		}
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
